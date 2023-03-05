c530b597e48d5d22eb369e21944b67e8a7ddb7a2
Trevor Sorel <twilliamsorel@gmail.com>
2023-03-05 09:49:31 -0700

    adding htmx

--- _src/assets/js/init.js
+++
@@ -1,5 +0,0 @@
-import HomePage from './pages/HomePage.js'
-
-if (/^\/$/.test(window.location.pathname)) {
-  HomePage();
-}
--- server/index.js
+++ server/index.js
@@ -12,37 +12,77 @@ app.use(cors());
 const db = new sqlite3.Database('./database/main.db');
 
 app.get('/api/shows/get-shows(/:page)?', (req, res, next) => {
-  const page = req.params.page;
+  const page = parseInt(req.params.page);
   const querySize = 10;
   const offset = page > 1 ? (page * querySize) - querySize : 0;
-  const sort = req.query.sort.replace(/(_asc|_desc)$/, '');
-  let genre = req.query.genre;
 
-  /*
-    let direction = 'desc';
-  
-    if (/asc$/.test(req.query.sort)) {
-      directon = 'asc';
-    }
-    */
+  db.all('select * from shows where review_score not like "no reviews" order by review_score desc limit ? offset ?', [querySize, offset], (err, data) => {
+    if (err) console.log(err);
 
+    res.set('Content-Type', 'text/html');
+    data.forEach((item, i) => {
+      const genres = item.genre ? item.genre.split(',') : null;
 
-  if (genre === 'all') {
-    db.all('select * from shows where review_score not like "no reviews" order by review_score desc limit ? offset ?', [querySize, offset], (err, data) => {
-      if (err) console.log(err);
-      res.send(data);
+      const html = `
+        <article class="component card">
+          ${i >= data.length - 1 ? `<div class="paginator" hx-get="http://localhost:3000/api/shows/get-shows/${page + 1}" hx-trigger="revealed" hx-target=".card-grid" hx-swap="beforeend"></div>` : ''}
+          <img src="${item.image}">
+          <header>
+            <h2>${item.title}</h2>
+            <span class="date">${item.release_date}</span>
+          </header>
+          <div class="body">
+            <p>${item.description}</p>
+            <div>rating: ${item.review_score} (${item.review_count})</div>
+            <div class="genres">
+              ${genres && genres.map((genre) => `<span class="genre-tag">${genre}</span>`).join('')}
+            </div>
+          </div>
+        </article>
+      `;
+
+      res.write(html);
     });
-  } else if (sort === 'review_score') {
-    db.all('select * from shows where instr(genre, ?) and review_score not like "no reviews" order by review_score desc limit ? offset ?', [genre, querySize, offset], (err, data) => {
-      if (err) console.log(err);
-      res.send(data);
+
+    res.end();
+  });
+});
+
+app.get('/api/movies/get-movies(/:page)?', (req, res, next) => {
+  const page = parseInt(req.params.page);
+  const querySize = 10;
+  const offset = page > 1 ? (page * querySize) - querySize : 0;
+
+  db.all('select * from movies where review_score not like "no reviews" order by review_score desc limit ? offset ?', [querySize, offset], (err, data) => {
+    if (err) console.log(err);
+
+    res.set('Content-Type', 'text/html');
+    data.forEach((item, i) => {
+      const genres = item.genre ? item.genre.split(',') : null;
+
+      const html = `
+        <article class="component card">
+          ${i >= data.length - 1 ? `<div class="paginator" hx-get="http://localhost:3000/api/movies/get-movies/${page + 1}" hx-trigger="revealed" hx-target=".card-grid" hx-swap="beforeend"></div>` : ''}
+          <img src="${item.image}">
+          <header>
+            <h2>${item.title}</h2>
+            <span class="date">${item.release_date}</span>
+          </header>
+          <div class="body">
+            <p>${item.description}</p>
+            <div>rating: ${item.review_score}</div>
+            <div class="genres">
+              ${genres && genres.map((genre) => `<span class="genre-tag">${genre}</span>`).join('')}
+            </div>
+          </div>
+        </article>
+      `;
+
+      res.write(html);
     });
-  } else if (sort === 'release_date') {
-    db.all('select * from shows where instr(genre, ?) and review_score not like "no reviews" order by release_date desc, review_score desc limit ? offset ?', [genre, querySize, offset], (err, data) => {
-      if (err) console.log(err);
-      res.send(data);
-    });
-  }
+
+    res.end();
+  });
 });
 
 app.listen(port, () => {
